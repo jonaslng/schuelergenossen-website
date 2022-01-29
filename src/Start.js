f5abc3b5684cad920992b72f4bc1eebe23db15be
Jonas Lang <jonasdelorean@gmail.com>
2022-01-29 21:44:49 +0100

    v1.0.4

--- src/Start.js
+++ src/Start.js
@@ -2,7 +2,21 @@ export default function Start(props) {
     
     return (
         <div className="start-wrapper">
-            
+            <img className="start-img" src="https://image.jimcdn.com/app/cms/image/transf/dimension=700x1024:format=jpg/path/s5716ff8e3a6d452a/image/ia2a80a50758a8e2f/version/1487693330/image.jpg"/>
+            <div className="start-txt1-wrapper">
+                <p className="start-txt1-header">Herzlich Wilkommen!</p>
+                <div className="start-txt1-description-wrapper">
+                    <p className="start-txt1-description">Mehr Informationen zu den Geschäftsbereichen "SGS Merchandise" und "Potato Company" erhältst du, wenn du auf das entsprechende Logo klickst.</p>
+                </div>
+                <div className="start-c-wrapper">
+                    <div className="start-c1">
+                        <img src="https://image.jimcdn.com/app/cms/image/transf/dimension=394x10000:format=png/path/s5716ff8e3a6d452a/image/i9cad7ab5f7f57ad5/version/1593595056/image.png" className="start-c1-logo"/> 
+                    </div>
+                    <div className="start-c2">
+                        <img src="https://image.jimcdn.com/app/cms/image/transf/none/path/s5716ff8e3a6d452a/image/ia2fe91a2e78d9c8c/version/1455131170/image.png" className="start-c2-logo"/> 
+                    </div>
+                </div>
+            </div>
         </div>
     )
 }
