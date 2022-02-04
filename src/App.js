0a7ef3433d1fbe924cc2b4c0d503763aa706cb72
Jonas Lang <jonasdelorean@gmail.com>
2022-02-04 19:17:37 +0100

    cleaned up

--- src/App.js
+++ src/App.js
@@ -9,12 +9,6 @@ import Team from "./Team"
 import Press from "./Press"
 
 function App() {
-  const openNav = () => {
-    document.getElementById("mySidenav").style.width = "250px";
-  }
-  const closeNav = () => {
-    document.getElementById("mySidenav").style.width = "0";
-  }
   const [site, setSite] = useState(1);
   const content = [
     <Start setSite={setSite}/>,
@@ -26,25 +20,7 @@ function App() {
 
   return (
     <div className="App">
-      <div className="navbar-web">
-        <div onClick={() => setSite(1)}><p>Home</p></div>
-        <div onClick={() => setSite(2)}><p>Shop</p></div>
-        <div onClick={() => setSite(3)}><p>Potato Company</p></div>
-        <div onClick={() => setSite(4)}><p>Team</p></div>
-        <div onClick={() => setSite(5)}><p>Presse</p></div>
-      </div>
-      <div className='navbar-mobile'>
-          <div id="mySidenav" className="sidenav-mobile">
-            <p className="closebtn" onClick={() => closeNav()}>&times;</p>
-            <p onClick={() => setSite(1)}>Home</p>
-            <p onClick={() => setSite(2)}>Shop</p>
-            <p onClick={() => setSite(3)}>Potato Company</p>
-            <p onClick={() => setSite(4)}>Team</p>
-            <p onClick={() => setSite(5)}>Presse</p>
-        </div>
-        <span className='material-icons md-36 md-dark' onClick={() => openNav()} color='primary'>menu</span>
-      </div>
-      {content[site-1]}
+      
     </div>
   );
 }
--- src/Start.js
+++ src/Start.js
@@ -2,19 +2,7 @@ export default function Start(props) {
     
     return (
         <div className="start-wrapper">
-            <img className="start-img" src="https://image.jimcdn.com/app/cms/image/transf/dimension=700x1024:format=jpg/path/s5716ff8e3a6d452a/image/ia2a80a50758a8e2f/version/1487693330/image.jpg"/>
-            <div className="start-txt1-wrapper">
-                <p className="start-txt1-header">Herzlich Willkommen!</p>
-                <div className="start-txt1-description-wrapper">
-                    <p className="start-txt1-description">Mehr Informationen zu den Geschäftsbereichen "SGS Merchandise" und "Potato Company" erhältst du, wenn du auf das entsprechende Logo klickst.</p>
-                </div>
-                <div className="start-c-wrapper">
-                        <img src="https://image.jimcdn.com/app/cms/image/transf/dimension=394x10000:format=png/path/s5716ff8e3a6d452a/image/i9cad7ab5f7f57ad5/version/1593595056/image.png" className="start-c1-logo" onClick={() => props.setSite(2)} title="SGS Merchandise"/> 
-                        <p className="start-c2-description"></p>
-                        <img src="https://image.jimcdn.com/app/cms/image/transf/none/path/s5716ff8e3a6d452a/image/ia2fe91a2e78d9c8c/version/1455131170/image.png" className="start-c2-logo" onClick={() => props.setSite(3)} title="Potato Company"/> 
-                        <p className="start-c1-description"></p>
-                </div>
-            </div>
+            
         </div>
     )
 }
