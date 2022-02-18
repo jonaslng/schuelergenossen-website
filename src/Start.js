6e1fb651c634509ed34c1a7db9fb42ef7a3907fd
Jonas Lang <jonasdelorean@gmail.com>
2022-02-18 21:38:30 +0100

    updated start page

--- src/Start.js
+++ src/Start.js
@@ -1,13 +1,22 @@
 export default function Start(props) {
-    
-    return (
-        <div className="start-wrapper">
-            <div className="start-header-text-wrapper">
-                <p className="start-header">Schülergenossen Selm eSG</p>
-                <p className="start-header-text">
-                    Wir begrüßen dich auf den Seiten der Schülergenossen Selm eSG! Informationen zu unseren beiden Geschäftsbereichen erhältst du, wenn du auf das jeweilige Logo klickst.
-                </p>
-            </div>
+  return (
+    <div className="start-wrapper">
+      <div className="front-wrapper">
+        <img
+          src="https://image.jimcdn.com/app/cms/image/transf/dimension=380x10000:format=png/path/s5716ff8e3a6d452a/image/i047f7ead75260886/version/1455016429/image.png"
+          className="front-logo"
+        />
+        <div className="front-header-wrapper">
+          <p>Schülergenossen Selm</p>
         </div>
-    )
+        <div className="front-text">
+          <p>
+            Herzlich Willkommen auf der Website der Schülergenossen Selm eSG!
+            Klicke auf eins der Logos um mehr über unsere beiden
+            Geschääftsbereiche herauszufinden!
+          </p>
+        </div>
+      </div>
+    </div>
+  );
 }
