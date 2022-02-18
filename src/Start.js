1533c97cc4aa78ff1be6f0009bb13253fd2d4dcd
Jonas Lang <jonasdelorean@gmail.com>
2022-02-18 22:05:41 +0100

    added business cards

--- src/Start.js
+++ src/Start.js
@@ -13,8 +13,24 @@ export default function Start(props) {
           <p>
             Herzlich Willkommen auf der Website der Schülergenossen Selm eSG!
             Klicke auf eins der Logos um mehr über unsere beiden
-            Geschääftsbereiche herauszufinden!
+            Geschäftsbereiche herauszufinden!
           </p>
+        </div>
+        <div className="businesses">
+          <div>
+            <img
+              src="https://image.jimcdn.com/app/cms/image/transf/dimension=380x10000:format=png/path/s5716ff8e3a6d452a/image/i047f7ead75260886/version/1455016429/image.png"
+              className="front-logo"
+            />
+            <p>SGS Merchandise</p>
+          </div>
+          <div>
+            <img
+              src="https://image.jimcdn.com/app/cms/image/transf/none/path/s5716ff8e3a6d452a/image/ia2fe91a2e78d9c8c/version/1455131170/image.png"
+              className="front-logo"
+            />
+            <p>Potato Company</p>
+          </div>
         </div>
       </div>
     </div>
